d4e051e5cb85bb220311f0efdda5a9c6c512acf6
cole <cole.galway@yahoo.com>
2025-09-17 12:01:28 -0400

    added a buddy and an address book and removed the buddy from the book.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,8 +4,8 @@ public class AddressBook {
 
     private ArrayList<BuddyInfo> addressBook;
 
-    public AddressBook(ArrayList<BuddyInfo> addressBook) {
-        this.addressBook = addressBook;
+    public AddressBook() {
+        addressBook = new ArrayList<>();
     }
 
     public void addBuddy(BuddyInfo buddy) {
@@ -19,6 +19,11 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Cole", "1125 Colonel By Drive", "613-111-1111");
+        AddressBook addressBook1 = new AddressBook();
+        addressBook1.addBuddy(buddy);
+        addressBook1.removeBuddy(buddy);
         System.out.println("Address Book");
+
     }
 }
